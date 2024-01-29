eb1aa72ac06fd4c7128f8871c5e1cc1488535c65
Daniel Alberski <danielalberski@gmail.com>
2024-01-29 22:12:21 +0100

    debug version 1

--- src/configs/deviceMonitor.config.ts
+++ src/configs/deviceMonitor.config.ts
@@ -1,15 +1,13 @@
-import waterLeakSensors from './waterLeak.config'
-
 type DeviceMetadata = {
   entityId: string
   name: string
 }
 
 const devices: DeviceMetadata[] = [
-  ...waterLeakSensors.map((wls) => ({
-    entityId: wls.entityId,
-    name: `${wls.name} - water leak sensor`,
-  })),
+  {
+    entityId: 'binary_sensor.waterfilterleaksensor_water_leak',
+    name: 'Water filter leak sensor',
+  },
   {
     entityId: 'binary_sensor.maindoordeadboltsensor_contact',
     name: 'Main door deadbolt',

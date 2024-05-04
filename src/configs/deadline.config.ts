bd995cbd43c27e92be6a3b800d658c45ae21dd79
Daniel Alberski <danielalberski@gmail.com>
2024-05-04 16:02:20 +0200

    new deadlines - car insurance and technical inspection

--- src/configs/deadline.config.ts
+++ src/configs/deadline.config.ts
@@ -10,19 +10,31 @@ const deadlines: DeadlineConfig[] = [
     label: 'water prefilters',
     entityId: 'input_datetime.kitchenfilterservice',
     interval: 120,
-    warningThreshold: 10,
+    warningThreshold: 5,
   },
   {
     label: 'membrane water filter',
     entityId: 'input_datetime.kitchenmembranefilterservice',
     interval: 485,
-    warningThreshold: 10,
+    warningThreshold: 5,
   },
   {
     label: 'mineralization water filter',
     entityId: 'input_datetime.kitchenfinalfilterservice',
     interval: 365,
-    warningThreshold: 10,
+    warningThreshold: 5,
+  },
+  {
+    label: 'car insurance',
+    entityId: 'input_datetime.carinsurance',
+    interval: 365,
+    warningThreshold: 12,
+  },
+  {
+    label: 'car technical inspection',
+    entityId: 'input_datetime.cartechnicalinspection',
+    interval: 365,
+    warningThreshold: 12,
   },
 ]
 

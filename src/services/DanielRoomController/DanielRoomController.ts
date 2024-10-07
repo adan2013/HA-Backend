65ad071d282e54a01f63d4fe05d22cdabf532ed6
Daniel Alberski <danielalberski@gmail.com>
2024-10-07 21:24:38 +0200

    all room lights button - DanielRoomController.ts

--- src/services/DanielRoomController/DanielRoomController.ts
+++ src/services/DanielRoomController/DanielRoomController.ts
@@ -1,6 +1,8 @@
 import Service from '../Service'
 import Entity from '../../entities/Entity'
 import Entities from '../../configs/entities.config'
+import { webSocketMessage } from '../../events/events'
+import WS_CMD from '../../connectors/wsCommands'
 
 const warmTemperature = 2000
 const warmNormalBrightness = 90
@@ -9,6 +11,21 @@ const warmDimBrightness = 25
 const daylightTemperature = 4500
 const daylightNormalBrightness = 175
 const daylightMaxBrightness = 255
+
+const externalButtons = {
+  danielRoom: {
+    on: '1_single',
+    off: '3_single',
+  },
+  kitchen: {
+    on: '2_single',
+    off: '3_single',
+  },
+  livingRoom: {
+    on: '2_double',
+    off: '6_single',
+  },
+}
 
 class DanielRoomController extends Service {
   private bedRemote = Entity.tuyaRemote(Entities.sensor.remote.danielBed)
@@ -32,6 +49,48 @@ class DanielRoomController extends Service {
       this.bedLight.setTemperature(daylightTemperature, daylightMaxBrightness),
     )
     this.bedRemote.onAnyShortPressCount(3, () => this.bedLight.turnOff())
+    this.bedRemote.onDoublePress(4, () => this.turnOnAllLights())
+    this.bedRemote.onHoldPress(4, () => this.turnOffAllLights())
+  }
+
+  private triggerAnotherRemote(remoteId: string, button: string) {
+    webSocketMessage(WS_CMD.incoming.REMOTE_CONTROL).emit({
+      message: {
+        id: remoteId,
+        value: button,
+      },
+      sendResponse: () => null,
+    })
+  }
+
+  private turnOnAllLights() {
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.danielBed,
+      externalButtons.danielRoom.on,
+    )
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.kitchen,
+      externalButtons.kitchen.on,
+    )
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.livingRoom,
+      externalButtons.livingRoom.on,
+    )
+  }
+
+  private turnOffAllLights() {
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.danielBed,
+      externalButtons.danielRoom.off,
+    )
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.kitchen,
+      externalButtons.kitchen.off,
+    )
+    this.triggerAnotherRemote(
+      Entities.sensor.remote.livingRoom,
+      externalButtons.livingRoom.off,
+    )
   }
 }
 
